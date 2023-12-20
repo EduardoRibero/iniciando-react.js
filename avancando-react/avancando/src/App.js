1a68c0962ddfebe287802ef952b4064eeab9119e
EduardoRibero <ribeirodomingues2020@gmail.com>
2023-12-19 21:37:03 -0300

    Imagens em assets

--- avancando-react/avancando/src/App.js
+++ avancando-react/avancando/src/App.js
@@ -1,15 +1,22 @@
-import logo from './logo.svg';
+
+import City from "./assets/city.jpg"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+
      <h1>Seção 3;</h1>
 
      {/*Imagem em public*/}
      <div>
         <img src="./img1.jpg" alt="Paisagem" />
      </div>
+
+     {/*Imagem em assets*/}
+    <div>
+      <img src={City} alt="Cidade" />
+    </div>
 
     </div>
   );
