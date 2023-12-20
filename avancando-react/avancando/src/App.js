4fa7e3155b09f555bd9f51b6b661c2df0956e7b7
EduardoRibero <ribeirodomingues2020@gmail.com>
2023-12-19 21:26:24 -0300

    Imagens

---
+++ avancando-react/avancando/src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h1>Seção 3;</h1>
+
+     {/*Imagem em public*/}
+     <div>
+        <img src="./img1.jpg" alt="Paisagem" />
+     </div>
+
+    </div>
+  );
+}
+
+export default App;
