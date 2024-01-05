566156ff064dadea30df457764e6560f2e5579f6
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-01-05 01:12:44 -0300

    CSS inline

--- css-react/css/src/App.jsx
+++ css-react/css/src/App.jsx
@@ -5,13 +5,24 @@ import MyComponent from './componentes/MyComponent';
 function App() {
   return (
     <div className="App">
+
+       {/*CSS global*/}
         <h1>React com CSS</h1>
+
+        {/*CSS Componente*/}
         <BotaoEntrada/>
+
         <button>
             ""CLICK AQUI 2 !""
         </button>
+
         <MyComponent/>
+
         <p>Paragrafo NÃO personalizado</p>
+
+        {/*CSS inline*/}
+        <p style={{color: "blue", padding: "25px", borderTop: "2px solid red"}}>Paragrafo do css inline</p>
+
     </div>
   );
 }
