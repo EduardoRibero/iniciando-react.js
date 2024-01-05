0d23ed079544a131d1c4fd94e61f1c90996245a1
EduardoRibero <ribeirodomingues2020@gmail.com>
2024-01-05 01:01:02 -0300

    css componentes

--- css-react/css/src/App.jsx
+++ css-react/css/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css';
 import BotaoEntrada from './componentes/BotaoEntrada';
+import MyComponent from './componentes/MyComponent';
 
 function App() {
   return (
     <div className="App">
         <h1>React com CSS</h1>
         <BotaoEntrada/>
+        <button>
+            ""CLICK AQUI 2 !""
+        </button>
+        <MyComponent/>
+        <p>Paragrafo NÃO personalizado</p>
     </div>
   );
 }
---
+++ css-react/css/src/componentes/MyComponent.jsx
@@ -0,0 +1,12 @@
+import './MyComponent.css';
+
+const MyComponent = () => {
+  return (
+    <div>
+        <h1>My component</h1>
+        <p>Paragrafo personalizado</p>
+    </div>
+  )
+}
+
+export default MyComponent
